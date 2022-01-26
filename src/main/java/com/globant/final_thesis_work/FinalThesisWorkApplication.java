ee5e882505f854482206c772696ecf8e9d122da3
Alex <alexander.cardenas@globant.com>
2022-01-25 22:46:38 -0500

    Initial Commit

---
+++ src/main/java/com/globant/final_thesis_work/FinalThesisWorkApplication.java
@@ -0,0 +1,13 @@
+package com.globant.final_thesis_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalThesisWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalThesisWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/globant/final_thesis_work/FinalThesisWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.final_thesis_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalThesisWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
